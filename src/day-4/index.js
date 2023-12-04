cd5de347b980e80c5e14c19c861ce17fe9ff361c
Aatu Nykänen <25407085+aanykanen@users.noreply.github.com>
2023-12-04 21:50:31 +0200

    Day 4 completed

---
+++ src/day-4/index.js
@@ -0,0 +1,61 @@
+function parseCardPoints(card) {
+  const cardId = Number(card.match(/^Card\s+(?<cardId>\d+): /).groups.cardId);
+  const cardInput = card.replace(/^Card\s+\d+: /, '');
+  const [winningNumbers, cardNumbers] = cardInput.split('|').map(parseCardInfo);
+  const { points, cardsWon } = getWinnings(cardId, cardNumbers, winningNumbers);
+
+  return { cardId, points, cardsWon };
+
+  function getWinnings(cardId, cardNumbers, winningNumbers) {
+    const correctNumbers = winningNumbers.filter((winningNumber) => cardNumbers.includes(winningNumber));
+    const points = correctNumbers.length === 0 ? 0 : 2 ** (correctNumbers.length - 1);
+    const cardsWon = Array.from({ length: correctNumbers.length }, (_, i) => i + 1 + cardId);
+    return { points, cardsWon };
+  }
+
+  function parseCardInfo(cardInput) {
+    return Array.from(cardInput.matchAll(/\d+/g)).map((v) => Number(v));
+  }
+}
+
+export function solvePuzzle1(puzzleInput) {
+  const cards = puzzleInput.split('\n');
+  return cards.map(parseCardPoints).reduce((p, n) => (n.points += p), 0);
+}
+
+export function solvePuzzle2(puzzleInput) {
+  const origCards = puzzleInput.split('\n').map(parseCardPoints);
+  // Note: reversing the order because acquired cards can only have greater ID than
+  // the card it was acquired from
+  const cardsAdded = origCards.reverse().reduce((p, n) => {
+    const { cardId, cardsWon } = n;
+    return [...p, { cardId, cardsAdded: calculateCards(cardsWon, p) }];
+  }, []);
+
+  return cardsAdded.reduce((p, n) => p + n.cardsAdded, 0);
+
+  function calculateCards(cardsWon, cardDict) {
+    if (cardsWon.length === 0) {
+      return 1;
+    }
+
+    return cardsWon.reduce((p, n) => p + cardDict.find((card) => card.cardId === n).cardsAdded, 0) + 1;
+  }
+
+  // My first attempt considered recursion, but this will run out of heap memory
+  /*
+  const origCards = puzzleInput.split('\n').map(parseCardPoints);
+  const origWinnings = origCards.map((c) => c.cardsWon).flat();
+  return recurseWinnings(origWinnings, origCards.length + origWinnings.length);
+
+  function recurseWinnings(cards, points) {
+    if (cards.length === 0) {
+      return points;
+    }
+
+    const [currentCardId, ...rest] = cards;
+    const { cardsWon } = origCards.find((card) => card.cardId === currentCardId);
+    return recurseWinnings([...rest, ...cardsWon], points + cardsWon.length);
+  }
+  */
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import * as day3 from './day-3';
+import * as day4 from './day-4';
 import { readPuzzleInput } from './utils';
 
 // TODO: cli usage with day as parameter
 async function run() {
-  const puzzleInput = await readPuzzleInput(3);
-  const result1 = await day3.solvePuzzle1(puzzleInput);
-  const result2 = await day3.solvePuzzle2(puzzleInput);
+  const puzzleInput = await readPuzzleInput(4);
+  const result1 = await day4.solvePuzzle1(puzzleInput);
+  const result2 = await day4.solvePuzzle2(puzzleInput);
 
   console.log('Result of puzzle 1: ', result1);
   console.log('Result of puzzle 2: ', result2);
